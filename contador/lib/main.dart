d0019daade01e44eb3ab953bca1178ec36620605
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2021-11-04 12:04:10 -0400

    ajust

--- contador/lib/main.dart
+++ contador/lib/main.dart
@@ -2,7 +2,8 @@ import 'dart:html';
 
 import 'package:flutter/cupertino.dart';
 //importaciones hechas
-import 'src/app.dart';
+//import 'src/app.dart';
+import 'package:contador/src/app.dart';
 
 void main() {
   runApp(MyApp());
