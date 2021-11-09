0256d431e8fce9e3ae07cfb66c1fe779ca5a90a2
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2021-11-09 09:14:19 -0400

    creando acciones

--- contador/lib/src/pages/contador_page.dart
+++ contador/lib/src/pages/contador_page.dart
@@ -14,27 +14,32 @@ class _ContadorPageState extends State<ContadorPage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text('StateFull'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Numero de taps:', style: _estiloTexto),
-            Text('$_conteo', style: _estiloTexto),
-          ],
+        appBar: AppBar(
+          title: Text('StateFull'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          //print('Hola mundo');
-          setState(() {
-            _conteo++;
-          });
-        },
-      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Numero de taps:', style: _estiloTexto),
+              Text('$_conteo', style: _estiloTexto),
+            ],
+          ),
+        ),
+        floatingActionButton: _crearBotones());
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30.0),
+        FloatingActionButton(child: Icon(Icons.exposure_zero), onPressed: null),
+        Expanded(child: SizedBox(width: 5.0)),
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: null),
+        SizedBox(width: 5.0),
+        FloatingActionButton(child: Icon(Icons.add), onPressed: null),
+      ],
     );
   }
 }
