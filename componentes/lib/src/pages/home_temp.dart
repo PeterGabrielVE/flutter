1e05bd429a01b309f6e3689a347a5c7057d1e320
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2021-11-27 22:33:33 -0400

    clase 63 List y ListLite

--- componentes/lib/src/pages/home_temp.dart
+++ componentes/lib/src/pages/home_temp.dart
@@ -1,7 +1,9 @@
+// ignore_for_file: prefer_collection_literals, unnecessary_new
+
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
-  const HomePageTemp({Key? key}) : super(key: key);
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
 
   @override
   Widget build(BuildContext context) {
@@ -10,20 +12,39 @@ class HomePageTemp extends StatelessWidget {
         title: const Text('Componentes Temp'),
       ),
       body: ListView(
+          //children: _crearItems()
+          children: _crearItemsCorta()),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = <Widget>[];
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget);
+      lista..add(Divider());
+    }
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
         children: <Widget>[
           ListTile(
-            title: Text('ListTile Title'),
+            title: Text(item + '!'),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.account_balance_wallet),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
           ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          )
+          Divider()
         ],
-      ),
-    );
+      );
+    }).toList();
   }
 }
